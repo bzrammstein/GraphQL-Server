43c1441955e0346214f1fc150d295df2bf3c3e5b
bzrammstein <bzrammstein@users.noreply.github.com>
2019-11-04 22:10:43 -0300

    Update resolvers.js

--- servidor/data/resolvers.js
+++ servidor/data/resolvers.js
@@ -17,8 +17,25 @@ export const resolvers = {
         },
     },
     Mutation: {
-        crearCliente : (root, args) => {
-            console.log(args);
+        crearCliente : (root, {input}) => {
+            const nuevoCliente = new Clientes({
+                nombre: input.nombre,
+                apellido: input.apellido,
+                empresa: input.empresa,
+                edad: input.edad,
+                tipo: input.tipo,
+                pedidos: input.pedidos,
+                emails: input.emails
+            });
+            nuevoCliente.id = nuevoCliente._id;
+            console.log(input);
+            
+            return new Promise((resolve, object) => {
+                nuevoCliente.save((error) => {
+                    if(error) rejects(error)
+                    else resolve(nuevoCliente)
+                });
+            });
         },
         actualizarCliente: (root, {input}) => {
                 return new Promise((resolve, object) => {
